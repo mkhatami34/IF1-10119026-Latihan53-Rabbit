eb088d1d9d34c34215f0242424842fdd08fefda3
mkhatami34 <koabarab361@gmail.com>
2020-11-22 01:49:53 +0700

    initial commit

---
+++ src/if1/pkg10119026/latihan53/rabbit/Animal.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119026.latihan53.rabbit;
+
+/**
+ *
+ * @author Asus
+ * NAMA         : Muhammad Khatami
+ * KELAS        : IF-1
+ * NIM          : 10119026
+ * Deskripsi Program : program ini untuk menampilkan deskripsi rabbit
+ */
+public class Animal {
+    protected boolean vegetarian;
+    protected String eats;
+    protected int noOfLegs;
+    
+    public Animal(boolean vegetarian, String food, int legs){
+        this.vegetarian = vegetarian;
+        this.eats = food;
+        this.noOfLegs = legs;
+    }
+
+    public boolean isVegetarian(){
+        return vegetarian;
+    }
+
+    public String getEats() {
+        return eats;
+    }
+
+    public int getNoOfLegs() {
+        return noOfLegs;
+    }
+    
+}
---
+++ src/if1/pkg10119026/latihan53/rabbit/Rabbit.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119026.latihan53.rabbit;
+
+/**
+ *
+ * @author Asus
+ * NAMA         : Muhammad Khatami
+ * KELAS        : IF-1
+ * NIM          : 10119026
+ * Deskripsi Program : program ini untuk menampilkan deskripsi rabbit
+ */
+public class Rabbit extends Animal{
+    private String color;
+    private String name;
+
+    public Rabbit(String name, boolean vegetarian, String food, int legs, String color) {
+        super(vegetarian, food, legs);
+        this.name = name;
+        this.color = color;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
